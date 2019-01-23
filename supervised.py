70395cce79577513f2c09bf3ff8c97448abf581d
SamVanderstraeten <31991068+SamVanderstraeten@users.noreply.github.com>
2019-01-23 10:53:43 +0100

    tt

--- supervised.py
+++ supervised.py
@@ -33,4 +33,66 @@ def run_test(model, path, show_images=False, test_folder='test'):
         count += 1
   print("Result for test: " + str(correct) + "/" + str(count) + "(" + str(correct/count) + ")")
   # TODO show more results?
+
+import os, random, shutil
+
+# Function can be executed when new data is added to 'samples', data will be added to already exisiting sets
+#
+# base dir must have 'samples' folder with images, thewe will be divided in train, validation & test set
+# divide contains division of (train, validation, test) set, like (0.8, 0.1, 0.1)
+# keep_samples True means samples will be COPIED instead of MOVED (so they also remain in the sample folder) TODO test this
+
+# TODO keep class folders intact or generate class folders based on filename
+def splitSamples(base, divide=(0.8,0.1,0.1), keep_samples=False):
+  if not base[-1] == "/":
+    base += "/"
     
+  # create folders if they don't exist already
+  train = os.path.join(base, "train")
+  valid = os.path.join(base, "valid")
+  test = os.path.join(base, "test")
+  sample = os.path.join(base, "samples")
+  
+  print(str(sample))
+  
+  if not os.path.exists(sample):
+    print("Nothing here...")
+    return
+    
+  if not os.path.exists(train):
+    os.makedirs(train)
+  if not os.path.exists(valid):
+    os.makedirs(valid)
+  if not os.path.exists(test):
+    os.makedirs(test)
+    
+  numSamples = len(os.listdir(sample))
+  print("# samples: " + str(numSamples))
+    
+  for img in os.listdir(sample):
+    r = random.uniform(0, 1)
+    target = train
+    if r > divide[0] + divide[1]:
+      target = test
+    elif r > divide[0]:
+      target = valid      
+      
+    orig_full_path = os.path.join(sample, img)
+    new_full_path = os.path.join(target, img)
+    print("Move to " + new_full_path)
+    if keep_samples:
+      copyfile(orig_full_path, new_full_path)
+    else:
+      os.rename(orig_full_path, new_full_path)
+      
+  return (train, valid, test)
+
+
+# counts number of features in all subfolder in a set (e.g. train set)
+def count_features(path):
+  count = 0
+  for subdir in os.listdir(path):
+    subdir_path = os.path.join(path, subdir)
+    for sp in os.listdir(subdir_path):
+      count += 1
+  return count
